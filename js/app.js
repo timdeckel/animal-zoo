a83e6bd0f4bd3cb92c55339be06cd4e2416b6b1f
bedirgcmz <bdrgcmz@gmail.com>
2024-01-15 21:14:38 +0100

    Created header / bedir

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("hello");
+// JavaScript ile active class'ı belirleme
+document.addEventListener("DOMContentLoaded", function () {
+  var currentLocation = window.location.href;
+
+  var navLinks = document.querySelectorAll(".main-navigation a");
+
+  navLinks.forEach(function (link) {
+    if (link.href === currentLocation) {
+      link.classList.add("active");
+    }
+  });
+});
